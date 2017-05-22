014567443ef57dde9dfb70436eb5bf145698908f
Georgi Kostov <gnkostov@yahoo.com>
2017-05-22 20:25:20 +0300

    define PhoneNumberTranslator logic

--- lib/phone_number_translator.ex
+++ lib/phone_number_translator.ex
@@ -1,2 +1,38 @@
 defmodule PhoneNumberTranslator do
+  def calculate(word) do
+    word
+    |> String.split("")
+    |> Enum.map(&PhoneNumberTranslator.match/1)
+  end
+
+  def match(character) do
+    criteria = fn (idx) -> Enum.member?(decipherer(idx), character) end
+
+    (2..9)
+    |> Enum.filter(criteria)
+    |> Enum.join("")
+  end
+
+  def print(word) do
+    word
+    |> PhoneNumberTranslator.calculate
+    |> IO.puts 
+  end
+
+  defp decipherer(index) do
+    helper = %{
+      2 => ["a", "b", "c"],
+      3 => ["d", "e", "f"],
+      4 => ["g", "h", "i"],
+      5 => ["j", "k", "l"],
+      6 => ["m", "n", "o"],
+      7 => ["p", "q", "r", "s"],
+      8 => ["t", "u", "v"],
+      9 => ["w", "x", "y", "z"]
+    }
+
+    helper[index]
+  end
 end
+
+PhoneNumberTranslator.print("bear")
