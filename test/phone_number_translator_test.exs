468c81282f84094f1922b1963cd1bd7b29ae8189
Georgi Kostov <gnkostov@yahoo.com>
2017-05-22 21:44:54 +0300

    add tests

--- test/phone_number_translator_test.exs
+++ test/phone_number_translator_test.exs
@@ -5,4 +5,13 @@ defmodule PhoneNumberTranslatorTest do
   test "the truth" do
     assert 1 + 1 == 2
   end
+
+  test "calculate beer" do
+    assert PhoneNumberTranslator.calculate("beer") == ["2", "3", "3", "7", ""]
+  end
+
+  test "calculate helloworld" do
+    result =["4", "3", "5", "5", "6", "9", "6", "7", "5", "3", ""]
+    assert PhoneNumberTranslator.calculate("helloworld") == result
+  end
 end
